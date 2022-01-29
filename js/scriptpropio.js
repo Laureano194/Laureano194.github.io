8fe563c64f0cdf7537c20befbb013daa98333c1e
Laureano Kronemberger <laureano.kronemberger@gmail.com>
2022-01-29 02:18:27 -0300

    puesta a punto

---
+++ js/scriptpropio.js
@@ -0,0 +1,22 @@
+const onChange = () => {
+    let celular = "542915049023";
+    let asistencia = document.getElementById("asistencia");
+    let nombre = document.getElementById("name").value.replace(" ", "%20");
+    let menu = document.getElementById("menu");
+    let cantidad = document.getElementById("cantidad");
+    let linkWhatsapp = "";
+    
+    if(asistencia.value=='No'){
+        menu.setAttribute("disabled", "true");
+        cantidad.setAttribute("disabled", "true");
+        linkWhatsapp = `https://api.whatsapp.com/send?phone=${celular}&text=Soy%20${nombre}%20,%20No%20voy%20chau%20`;
+    } else {
+        menu.removeAttribute("disabled");
+        cantidad.removeAttribute("disabled");
+        linkWhatsapp = `https://api.whatsapp.com/send?phone=${celular}&text=Soy%20${nombre}%20,%20somos%20${cantidad.value},%20menu%20${menu.value}`;
+    }
+    console.log(linkWhatsapp);
+    let botonAsistencia = document.getElementById("asistencia");
+    botonAsistencia.href = linkWhatsapp;
+    console.log(botonAsistencia.href);
+}
